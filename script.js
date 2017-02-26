d17254e213775fa371727b3dcecd487192ca5023
Justin Brunner <brunner.justin.jakob@gmail.com>
2017-02-25 20:11:37 -0700

    Small changes made
    Added some drag and drop functionality, also got the randomizing to
    happen on page load

--- script.js
+++ script.js
@@ -24,28 +24,29 @@ app.controller('puzzleCtrl', function puzzleController($scope) {
     };
 
     $scope.randomizeImgPositions = function() {
-        
        for (var numbers = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10], i = numbers.length; i--; ) {
             var random = numbers.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
            $scope.imgPcs[i - 1].position = random;
             console.log(random);
        }
-        //code to randomly rearrange the <img>'s so their position='i' is not the same as their div parent
-    }
-        
-window.onload=function(){  
-    var imagediv = document.getElementsByClassName("puzzle");
-    var imgarray = new Array("image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg");
-    var spot = Math.floor(Math.random()*imgarray.length);
-    for(var i=0;i<imagediv.length;i++){
-        imagediv[i].style.backgroundImage="url(" + imgarray[spot] + ")";
-
     }
 
-    //Some code to be able to drag and drop...
 
-    $scope.createPuzzle();
-    $scope.randomizeImgPositions();
+    $scope.init = function() {
+        var imagediv = document.getElementsByClassName("puzzle");
+        var imgarray = new Array("image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg");
+        var spot = Math.floor(Math.random()*imgarray.length);
+        for(var i=0;i<imagediv.length;i++){
+            imagediv[i].style.backgroundImage="url(" + imgarray[spot] + ")";
+
+        }
+
+        $scope.onDropComplete = function(data, event) {
+            console.log(data, event, this);
+        }
+
+        $scope.createPuzzle();
+        $scope.randomizeImgPositions();
     };
-    
+
 })
